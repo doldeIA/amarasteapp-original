191e869034abe70bc1f87e79ae704db0b7aff518
Do Outro Lado do Espelho <contato@dolde.com.br>
2025-08-12 09:34:43 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Inclui todos os arquivos React/Vite
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'red-white-stripes': 'repeating-linear-gradient(90deg, #c90000, #c90000 10px, #ffffff 10px, #ffffff 20px)',
+      },
+      colors: {
+        primary: '#A13500',
+        accent: '#FF8345',
+        'coke-red': '#E41B17',
+        'warm-brown': '#4B2E2E',
+        'booker-pink': '#FADADD',
+        'booker-beige': '#FBF3E4',
+        'booker-salmon': '#F8AFA6',
+        'booker-dark-salmon': '#E57373',
+        'crimson-red': '#B22222',
+        'soft-beige': '#fdf6ec',
+        'neon-fuchsia': '#e400ff',
+        'gold': '#D4AF37',
+      }
+    }
+  },
+  plugins: [],
+};
